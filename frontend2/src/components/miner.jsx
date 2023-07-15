96f9cf5a5427f6b71d8d98b7d6847e387758754a
mateonoel2 <mateo.noel@utec.edu.pe>
2023-07-15 00:49:14 -0500

    more fixes

--- frontend2/src/components/miner.jsx
+++ frontend2/src/components/miner.jsx
@@ -2,20 +2,21 @@ import axiosInstance from "./api";
 import React from "react";
 
 
-function Miner({user}) {
+function Miner({ user }) {
+  const mine = () => {
+    console.log("Mining...");
+    axiosInstance
+      .get(`/mine/${user.sub}`)
+      .then(response => response.data)
+      .then(text => console.log(text))
+      .catch(error => console.error(error)); 
+  };
 
-    const mine = () => {
-        console.log("Mining...");
-        axiosInstance.get(`/mine/${user.sub}`, { withCredentials: true }).then(response => response.data).then(text => console.log(text));
-    }
-
-    return(
-        <div style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
-        <button onClick={mine} >
-          Mine block
-        </button>
-      </div>
-    )
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
+      <button onClick={mine}>Mine block</button>
+    </div>
+  );
 }
 
 export default Miner;
